3178cd89ea1c4676fe49c246e654830482552d1e
Steven Bax <sbax@yourownmood.nl>
2018-09-22 17:05:46 +0200

    Handles time null correctly

--- src/components/TimeFromNow/TimeFromNow.spec.tsx
+++ src/components/TimeFromNow/TimeFromNow.spec.tsx
@@ -11,3 +11,8 @@ it('renders without crashing', () => {
   const component = shallow(<TimeFromNow {...props} />);
   expect(component.find('.c-time-from-now')).toHaveLength(1);
 });
+
+it('render a "null" time correctly', () => {
+  const component = shallow(<TimeFromNow time={null} />);
+  expect(component.find('.c-time-from-now').text()).toBe('...');
+});
--- src/components/TimeFromNow/TimeFromNow.tsx
+++ src/components/TimeFromNow/TimeFromNow.tsx
@@ -2,15 +2,19 @@ import * as moment from 'moment';
 import * as React from 'react'
 
 interface InterfaceProps {
-  time: string,
+  time: string | null,
 }
 
 class TimeFromNow extends React.Component<InterfaceProps> {
   public convertTime = () => {
     const { time } = this.props;
-    const timeConvertTimezone = moment(time).utcOffset(time);
-    const timeFromNow = moment(timeConvertTimezone, 'YYYY-MM-DDTHH:mm:ss.SSSSZ').fromNow();
-    return timeFromNow;
+    if (time) {
+      const timeConvertTimezone = moment(time).utcOffset(time);
+      const timeFromNow = moment(timeConvertTimezone, 'YYYY-MM-DDTHH:mm:ss.SSSSZ').fromNow();
+      return timeFromNow;
+    }
+
+    return '...';
   }
 
   public render() {
